6743af814e77483bbf2a35f378d329a08a522da1
Ryan Cobb <ryan.david.cobb@gmail.com>
2016-03-15 22:29:03 -0600

    change to relative assets

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,16 +1,1 @@
-// This is where it all goes :)
-$(function() {
-
-  var $root = $('html, body');
-  $('.nav li a').click(function(){
-      var href = $.attr(this, 'href');
-      $root.animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top - 50
-      }, 500, function () {
-        window.location.hash = href;
-      });
-      return false;
-  });
-
-
-});
+$(function(){var t=$("html, body");$(".nav li a").click(function(){var n=$.attr(this,"href");return t.animate({scrollTop:$($.attr(this,"href")).offset().top-50},500,function(){window.location.hash=n}),!1})});
